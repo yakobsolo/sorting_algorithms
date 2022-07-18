e2e62cf29c7b3b26dd22210de549b7e4f7d7f6b5
user name <user email>
2022-07-18 16:20:11 +0000

    quick sort

--- sort.h
+++ sort.h
@@ -14,4 +14,7 @@ void print_list(const listint_t *list);
 void swap_nodes(listint_t **list, listint_t *nodeA, listint_t *nodeB);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void lomuto_qsort(int *array, int lo, int hi, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
 #endif
