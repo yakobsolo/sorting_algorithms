a11a621497b49beb25f7658d455aa0e9adb9a97f
user name <user email>
2022-07-18 07:22:11 +0000

    function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,7 +10,7 @@ void selection_sort(int *array, size_t size)
 	size_t i, curritem, currmin;
 	int temp;
 
-	if (size < 2)
+	if (array == NULL && size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
